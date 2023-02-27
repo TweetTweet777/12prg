9f73bd0137026a7f206b0eb72abc2066bdc78bde
Max Ly <48974347+TweetTweet777@users.noreply.github.com>
2023-02-28 11:02:31 +1300

    finished cycling

--- Programming Challenges/Section 1/Cycling.py
+++ Programming Challenges/Section 1/Cycling.py
@@ -1,7 +1,7 @@
 import math
-circ = input("What is the circumference of your wheel in milimeters?")
-rev = input("How many wheel revolutions have taken place in your journey?")
-mins = input("How many minutes did you cycle for?")
+circ = int(input("What is the circumference of your wheel in milimeters?"))
+rev = int(input("How many wheel revolutions have taken place in your journey?"))
+mins = int(input("How many minutes did you cycle for?"))
 
 distance = (circ * rev) / 1000000
 speed = distance/(mins/60)
