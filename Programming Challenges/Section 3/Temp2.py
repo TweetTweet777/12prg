0f72fe3c128a8dc51000d9c37ece5823f19576e4
Max Ly <tweettweetmaxximo@gmail.com>
2023-03-10 15:02:23 +1300

    edited temp2

--- Programming Challenges/Section 3/Temp2.py
+++ Programming Challenges/Section 3/Temp2.py
@@ -1,10 +1,11 @@
-
 for loop in range(4):
     print("Week", loop + 1)
     print("Please enter the seven tempuratures.")
     total = 0
-    for rep in range(7):
+    
+    for count in range(7):
         temp = int(input())
+        
         while (temp > 55) or (temp < -40):
             print("Tempurature should be between -40 and 55")
             temp = int(input())
