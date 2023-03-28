cc8fcd559fbeb237ac23138f09faa5b4582f7673
Max Ly <tweettweetmaxximo@gmail.com>
2023-03-28 15:02:32 +1300

    Update Vowels.py

--- Programming Challenges/Section 1/Vowels.py
+++ Programming Challenges/Section 1/Vowels.py
@@ -1,10 +1,10 @@
 sentence = input("Please enter a sentence of your choice.")
 sentence = sentence.lower()
-a = sentence.count("a");
-e = sentence.count("e");
-i = sentence.count("i");
-o = sentence.count("o");
-u = sentence.count("u");
+a = sentence.count("a")
+e = sentence.count("e")
+i = sentence.count("i")
+o = sentence.count("o")
+u = sentence.count("u")
 print("Your sentence contained the following:")
 print("a =", a)
 print("e =", e)
