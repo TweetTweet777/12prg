e8be7ad8030c4143afdf5ac493c9861ed3028d59
Max Ly <tweettweetmaxximo@gmail.com>
2023-04-05 15:06:45 +1200

    rgdr

--- Programming Challenges/Section 5/Dice.py
+++ Programming Challenges/Section 5/Dice.py
@@ -3,7 +3,7 @@ list = []
 count = 0
 while not len(list) == 6:
     count = count + 1
-    rolled = random.randint(1,7)
+    rolled = random.randint(1,6)
     print("Dice Rolled =", rolled)
     if list.count(rolled) >= 1:
         list.remove(rolled)
